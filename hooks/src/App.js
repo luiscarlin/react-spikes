2d3ee86e1ff889f95d67d5941079b02d5bd6d1fd
Luis Carlin <luiscarlin@gmail.com>
2019-03-05 06:30:16 -0500

    first react hook

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Input from './Input'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <header>
-
-        </header>
-
+        <Input/>
       </div>
     );
   }
---
+++ hooks/src/Input.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+export default () => {
+  // value = You can name it whatever you want. The state.
+  // setValue = You can name it whatever you want. Function that mutates the state. By default it just sets it.
+  // useState = returns an array of state & mutator. Takes in the initial state.
+  const [value, setValue] = useState('')
+
+  // use setValue() to update the internal state
+  return (
+    <>
+      <h1>Current Value is="{value}"</h1>
+      <input type="text" onChange={event => setValue(event.target.value)} value={value}/>
+    </>
+  )
+}
